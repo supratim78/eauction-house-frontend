ebd61aa9c2ad86c317a3b12986f2990f1a5f2d20
supratim78 <supratim78@gmail.com>
2022-07-07 12:34:32 +0530

    t

--- src/api/EAuctionDataService.js
+++ src/api/EAuctionDataService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class EAuctionDataService {
     retrieveAllBids(productId) {
         //console.log('executed service')
-        return axios.get(`http://54.214.122.197:8081/e-auction/api/v1/seller/show-bids/${productId}`);
+        return axios.get(`https://02k3bnf8ah.execute-api.us-west-2.amazonaws.com/e-auction/api/v1/seller/show-bids/${productId}`);
     }
 
     }
